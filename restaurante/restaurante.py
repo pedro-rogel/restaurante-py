566515a371b9efd9f56426026588ce0a262655cb
Pedro Rogel <pedrorogel3@gmail.com>
2024-11-06 23:05:47 -0300

    Criando as Tabelas

---
+++ restaurante/restaurante.py
@@ -0,0 +1,113 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, Date, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+
+engine = create_engine('sqlite:///restaurante.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Restaurante(Base):
+    __tablename__ = "restaurante"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    endereco = Column(String(50))
+    telefone = Column(String(13))
+    ramo = Column(String(20))
+    
+    def __init__(self, nome, endereco, telefone, ramo) -> None:
+        self.nome = nome
+        self.endereco = endereco
+        self.telefone = telefone
+        self.ramo = ramo
+
+
+class Cliente(Base):
+    __tablename__ = "clientes"
+    cpf = Column(String, primary_key=True)
+    nome = Column(String(50))
+    telefone = Column(String(13))
+    email = Column(String(50))
+    
+    # Relacionamento com Pedido
+    pedidos = relationship('Pedido', back_populates='cliente', cascade='all, delete-orphan')
+    
+    def __init__(self, nome, telefone, email) -> None:
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+
+
+class Funcionario(Base):
+    __tablename__ = 'funcionarios'
+    cpf = Column(String, primary_key=True)
+    nome = Column(String(50))
+    cargo = Column(String(50))
+    salario = Column(Float)
+    
+    def __init__(self, nome, cargo, salario) -> None:
+        self.nome = nome
+        self.cargo = cargo
+        self.salario = salario
+
+
+class Dono(Funcionario):
+    __tablename__ = 'donos'
+    id = Column(Integer, primary_key=True)
+    telefone = Column(String(13))
+    email = Column(String(50))
+    
+    def __init__(self, nome, cargo, salario, telefone, email) -> None:
+        super().__init__(nome, cargo, salario)
+        self.telefone = telefone
+        self.email = email
+
+
+class Cardapio(Base):
+    __tablename__ = 'cardapios'
+    id = Column(Integer, primary_key=True)
+    descricao = Column(String(50))
+    
+    # Relacionamento com Itens de Cardapio
+    itens = relationship("ItemCardapio", back_populates="cardapio")
+
+    def __init__(self, descricao) -> None:
+        self.descricao = descricao
+
+
+class ItemCardapio(Base):
+    __tablename__ = 'itens_cardapio'
+    
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    descricao = Column(String(50))
+    preco = Column(Float)
+    categoria = Column(String(50))
+    disponibilidade = Column(Boolean)
+    cardapio_id = Column(Integer, ForeignKey('cardapios.id'))
+    
+    # Relacionamento com Cardapio
+    cardapio = relationship("Cardapio", back_populates="itens")
+
+    def __init__(self, nome, descricao, preco, categoria, disponibilidade) -> None:
+        self.nome = nome
+        self.descricao = descricao
+        self.preco = preco
+        self.categoria = categoria
+        self.disponibilidade = disponibilidade
+
+
+class Pedido(Base):
+    __tablename__ = 'pedidos'
+    
+    id = Column(Integer, primary_key=True)
+    cliente_id = Column(String, ForeignKey('clientes.cpf'))
+    status = Column(String(50))
+    data_pedido = Column(Date)
+    
+    # Relacionamento com Cliente
+    cliente = relationship("Cliente", back_populates="pedidos")
+    
+    def __init__(self, cliente_id, status, data_pedido) -> None:
+        self.cliente_id = cliente_id
+        self.status = status
+        self.data_pedido = data_pedido
